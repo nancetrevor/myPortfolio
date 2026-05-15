a54666acd7ae95c7ae513f990bd680477bfa14e4
Trevor Nance <trevornance7@gmail.com>
2026-05-15 18:51:34 -0400

    added in blog html and fixed up handlers

--- internal/handlers/blog_handler.go
+++ internal/handlers/blog_handler.go
@@ -1,13 +1,25 @@
 package handlers
 
 import (
+	"fmt"
+	"html/template"
 	"net/http"
-	"text/template"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/lfizzikz/portfolioSite/package/markdown"
 )
 
-type BlogPageData struct {
+type BlogPage struct {
 	Title     string
-	BlogPosts []string
+	BlogPosts []BlogPostData
+}
+
+type BlogPostData struct {
+	Title   string
+	Date    string
+	Content string
 }
 
 func BlogHandler(w http.ResponseWriter, r *http.Request) {
@@ -16,13 +28,52 @@ func BlogHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Template error", http.StatusInternalServerError)
 		return
 	}
-	h := BlogPageData{
-		Title: "Blog",
-	}
 
-	t.Execute(w, h)
+	posts := parseMdToBlogPosts()
+
+	t.Execute(w, posts)
 }
 
-// func parseMdToBlogPosts() BlogPageData {
-//
-// }
+func parseMdToBlogPosts() BlogPage {
+	dir := "content/blog"
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		fmt.Println("error: ", err)
+		return BlogPage{}
+	}
+
+	page := BlogPage{}
+
+	for _, file := range files {
+		file := filepath.Join(dir, file.Name())
+		contents, err := os.ReadFile(file)
+		if err != nil {
+			fmt.Println("error: ", err)
+			return BlogPage{}
+		}
+
+		post, err := parseMdFile(string(contents))
+		if err != nil {
+			fmt.Println("error: ", err)
+			return BlogPage{}
+		}
+		page.BlogPosts = append(page.BlogPosts, post)
+	}
+	return page
+}
+
+func parseMdFile(text string) (BlogPostData, error) {
+	lines := strings.Split(text, "\n")
+	post := BlogPostData{}
+
+	post.Title = strings.TrimPrefix(lines[0], "title: ")
+	post.Date = strings.TrimPrefix(lines[1], "date: ")
+
+	body := strings.Join(lines[3:], "\n")
+
+	html := markdown.MdToHTML([]byte(body))
+
+	post.Content = string(html)
+
+	return post, nil
+}
